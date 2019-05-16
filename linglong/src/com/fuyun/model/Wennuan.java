bdc1756376de948b8b0d9d1978e8045c2a635898
liwereryimiliu <2367360777@qq.com>
2019-05-16 09:09:58 +0800

    五月十六初始化工程

---
+++ linglong/src/com/fuyun/model/Wennuan.java
@@ -0,0 +1,51 @@
+package com.fuyun.model;
+
+public class Wennuan {
+	
+		private int id;
+		private String username;
+		private String password;
+		private int age;
+		private String sex;
+		
+		
+		public int getId() {
+			return id;
+			
+		}
+		public void setId(int id) {
+			this.id=id;
+		}
+		public String getUsername() {
+			return username;
+			
+		}
+		public void setUsername(String username) {
+			this.username=username;
+		}
+		public String getPassword() {
+			return password;
+			
+		}
+		public void setPassword(String password) {
+			this.password=password;
+		}
+		public int getAge() {
+			return age;
+			
+		}
+		public void setAge(int age) {
+			this.age=age;
+		}
+		public String getSex() {
+			return sex;
+			
+		}
+		public void setSex(String sex) {
+			this.sex=sex;
+		}
+
+	
+
+
+}
